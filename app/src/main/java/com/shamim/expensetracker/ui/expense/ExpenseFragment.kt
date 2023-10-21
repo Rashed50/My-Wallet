9efd1a09879fff77f1891ff275b11d0bec1755e3
shamimcse1 <mdshamimh529@gmail.com>
2023-10-21 20:45:25 +0600

    Initial commit

---
+++ app/src/main/java/com/shamim/expensetracker/ui/expense/ExpenseFragment.kt
@@ -0,0 +1,42 @@
+package com.shamim.expensetracker.ui.expense
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.shamim.expensetracker.databinding.FragmentExpenseBinding
+
+
+class ExpenseFragment : Fragment() {
+
+    private var _binding: FragmentExpenseBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val expenseViewModel = ViewModelProvider(this)[ExpenseViewModel::class.java]
+
+        _binding = FragmentExpenseBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        expenseViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
